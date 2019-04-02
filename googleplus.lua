eb0a49743a8a60febed2a353f6dede5013b328ed
Arkiver2 <arkiver@protonmail.com>
2019-04-02 16:50:30 +0200

    Update version to 20190402.02; rewrite bad comevents URL.

--- googleplus.lua
+++ googleplus.lua
@@ -243,7 +243,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
     end
     for newurl in string.gmatch(string.gsub(html, "&quot;", '"'), '([^"]+)') do
       if string.match(newurl,"^//plus.google.comevents/") then
-        io.stdout:write("L223: " .. newurl .. "\n")
+        checknewurl(string.gsub(newurl, "comevents", "com/events"))
       else
         checknewurl(newurl)
       end
